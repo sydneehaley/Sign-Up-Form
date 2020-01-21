ba94cf68b64bd1243de3c856173a0f5140f677fc
sydneehaley <inquire@sydneehaley.com>
2020-01-20 21:19:16 -0500

    js file addes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,57 @@
-const form = document.getElementById('form');
-const firstName = document.getElementById('firstname');
-const lastName = document.getElementById('lastname');
-const email = document.getElementById('email');
+const form = document.getElementById('main__form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email-address');
 const password = document.getElementById('password');
 
 
-if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an e-mail address!");
-  } else {
-    email.setCustomValidity("");
-  }
+form.addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+
+    if (firstName.value === '') {
+        makeError(firstName, 'This field cannot be empty');
+    } else {
+        makeSuccess(firstName)
+    }
+
+    
+    if (lastName.value === '') {
+        makeError(lastName, 'This field cannot be empty');
+    } else {
+        makeSuccess(lastName);
+    }
+    if (email.value === '') {
+        makeError(email, 'Must provide an email');
+    } else if(isEmail(email.value) === false) {
+        makeError(email, 'Please enter your email address');
+    } 
+    else {
+        makeSuccess(email);
+    }
+    if(password.value === '') {
+        makeError(password, 'Must provide a password');
+    } else {
+        makeSuccess(password)
+    }
+})
+
+
+
+function makeError(input, message) {
+    const inputContainer = input.parentElement;
+    const small = inputContainer.querySelector('small');
+    inputContainer.className = 'input-field invalid';
+    input.placeholder = '';
+    small.innerText = message;
+}
+function makeSuccess(input) {
+    const inputContainer = input.parentElement;
+    inputContainer.className = 'input-field'; 
+}
+
+function isEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email); 
+}
