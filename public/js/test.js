ea86eb164e19f35e990cc657448afa320273dae1
sydneehaley <inquire@sydneehaley.com>
2020-02-25 22:07:11 -0500

    general update

--- public/js/test.js
+++ public/js/test.js
@@ -1,179 +1,45 @@
+const form = document.querySelector('form');
+let fields = Array.from(document.querySelectorAll('input'));
 
-const form = document.getElementById('main__form');
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
-const email = document.getElementById('email-address');
-const password = document.getElementById('password');
-const cfpassword = document.getElementById('confrmpassword');
-
-const pwletter = document.getElementById("letter");
-const pwcapital = document.getElementById("capital");
-const pwnumber = document.getElementById("number");
-const pwlength = document.getElementById("length");
-
-const lowerCaseLetters = /[a-z]/g;
-const upperCaseLetters = /[A-Z]/g;
-const numbers = /[0-9]/g;
-
-
-
-form.addEventListener('submit', function(event){
-    
-    event.preventDefault();
-
-    if (firstName.value === '') {
-        makeError(firstName, 'This field is required.');
-        changePlaceholder();
-    } else {
-        makeSuccess(firstName)
-    }
-
-    
-    if (lastName.value === '') {
-        makeError(lastName,'This field is required.');
-        changePlaceholder();
-    } else {
-        makeSuccess(lastName);
-    }
-    if (email.value === '') {
-        makeError(email,'This field is required.');
-        changePlaceholder();
-    } else if(correctEmail(email.value) === false) {
-        makeError();
-    } 
-    else {
-        makeSuccess(email);
-    }
-    if(password.value === '') {
-        makeError(password,'Please create an password.');
-        changePlaceholder();
-    } else {
-        makeSuccess(password)
-    }
-
-    if(cfpassword.value == password.value) {
-        makeSuccess(cfpassword)}
-        else{
-          makeError(cfpassword,'Password do not match.')}
-  })
-
-
-function makeError(input, err) {
-    const inputBlock = input.parentElement;
-    const small = inputBlock.querySelector('span');
-    inputBlock.className = 'input-field invalid';
-    input.placeholder = '';
-    small.innerText = err;
+// validate form
+function validateForm(event) {
+    // validate each input field
+    fields.forEach((field) => {
+        let name = field.name;
+        let id = field.id;
+        let error = document.querySelector(`#${id}Error`);
+        let isValid = field.validity.valid;
+        // add error state on invalid input
+        if (!isValid) {
+            event.preventDefault();
+            field.style.borderColor = 'red';
+            field.style.background = 'url("./images/icon-error.svg") no-repeat 98%';
+            // display error messages
+            error.textContent = "Please enter a valid " + name;
+            error.classList.add("errormsg");
+            if (field.validity.typeMismatch) error.textContent = "Please provide an accurate " + name;
+            if (field.validity.valueMissing) error.textContent = "Please provide your " + name;
+            if (field.validity.tooShort) error.textContent = name + " must be min " + field.getAttribute("minlength") + " characters";
+            // change email placeholder on invalid input
+            if (name = 'Email') {
+                let email = document.querySelector('#email');
+                email.placeholder = "email@yourdomain.com";
+            }
+        }
+        // reset form on submit
+        else {
+            error.textContent = '.';
+            field.style.borderColor = 'inherit';
+            field.style.background = 'none';
+        }
+        // reset error state on field input
+        field.oninput = function () {
+            error.textContent = '.';
+            field.style.borderColor = 'initial';
+            field.style.background = 'none';
+        }
+    });
 }
 
-function makeSuccess(input) {
-  const inputContainer = input.parentElement;
-  inputContainer.className = 'input-field'; 
-}
-
-   function changePlaceholder(){
-    var fnameph = document.getElementById('first-name').placeholder = "Please provide your first name";
-    var lnameph = document.getElementById('last-name').placeholder = "Please provide your last name";
-    var emailph = document.getElementById('email-address').placeholder = "Please provide an email address.";
-    var pwph = document.getElementById('password').placeholder = "Please create a password";
-
-  }
-
-
-function correctEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    // supplies values for a valid email address
-
-    return re.test(email); 
-}
-
-
-
-
-
-
-
-// When the user clicks on the password field, show the message box
-password.onfocus = function popuppwBox() {
-  var popup = document.getElementById("popupBox");
-  popup.classList.toggle("show");
-  }
-  
-  
-  
-  // When the user starts to type something inside the password field
-  password.onkeyup = function() {
-    // Validate lowercase letters
-  
-    if(password.value.match(lowerCaseLetters)) {
-      pwletter.classList.remove("wrongpw");
-      pwletter.classList.add("validpassw");
-    } else {
-      pwletter.classList.remove("validpassw");
-      pwletter.classList.add("wrongpw");
-  }
-  
-    // Validate capital letters
-    
-    if(password.value.match(upperCaseLetters)) {
-      pwcapital.classList.remove("wrongpw");
-      pwcapital.classList.add("validpassw");
-    } else {
-      pwcapital.classList.remove("validpassw");
-      pwcapital.classList.add("wrongpw");
-    }
-  
-    // Validate numbers
-    
-    if(password.value.match(numbers)) {
-      pwnumber.classList.remove("wrongpw");
-      pwnumber.classList.add("validpassw");
-    } else {
-      pwnumber.classList.remove("validpassw");
-      pwnumber.classList.add("wrongpw");
-    }
-  
-    // Validate length
-    if(password.value.length >= 8) {
-      pwlength.classList.remove("wrongpw");
-      pwlength.classList.add("validpassw");
-    } else {
-      pwlength.classList.remove("validpassw");
-      pwlength.classList.add("wrongpw");
-    }
-  
-
-  }
-
-  
-
-  function togglePassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-
-   
-  }
-
-  function togglecPassword() {
-    var x = document.getElementById("confrmpassword");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-
-   
-  }
-
-  
-               
-             
-                  
-            
-                
-                
+// validate form on submit
+form.addEventListener('submit', validateForm);
